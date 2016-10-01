be04343f93563f6b61efeae5de1d78a119decbc1
JohnPaulRyan24 <jpr349@nyu.edu>
2016-09-30 20:31:39 -0400

    added toggles

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Mandelbrot
+This is a program written in MATLAB to view and zoom into the Mandelbrot/Julia sets.
+
+To toggle between Mandelbrot and Julia, click below the image. To toggle the colormap, click above the image. To reset the zoom, click right of the image. To quit, click left of the image.
+
--- f.m
+++ f.m
@@ -3,22 +3,30 @@
 % This function returns the number of iterations (if less than 10,000)
 % it takes for repeated iteration of f(z) = z^2 + c
 % to diverge (have magnitude greater than 2)
-function value = f(a, b)
-
-a = a/1000.0; %Scaling somehow for now, not much thought
-b = b/1000.0;
-
-for i = 1:1000
-    %fprintf('a is %f and b is %f\n',a, b); 
-    mag = a^2 + b^2;
-    %fprintf('mag is %f\n',mag);
-    if(mag>2)
-        %fprintf('mag too big, returning %f\n', i);
-        value = i;
-        return;
+function value = f(a, b, julia, window_size)
+    if(julia)
+        a = 2*a/(window_size); %Scaling somehow for now, not much thought
+        b = 2*b/(window_size);
+        c_x = -0.79;
+        c_y = 0.15;
+    else
+        c_x = 2*a/(window_size); %Scaling somehow for now, not much thought
+        c_y = 2*b/(window_size);
+        a=0;
+        b=0;
     end
-    temp = a;
-    a = a^2 - b^2 - 0.79; %c is 0.5 + 0.5i
-    b = 2*temp*b  + 0.15;
-end
-value = 1000;
+    for i = 1:1000
+        %fprintf('a is %f and b is %f\n',a, b); 
+        mag = a^2 + b^2;
+        %fprintf('mag is %f\n',mag);
+        if(mag>2)
+            %fprintf('mag too big, returning %f\n', i);
+            value = i;
+            return;
+        end
+        temp = a;
+
+        a = a^2 - b^2 + c_x;% - 0.79; %c is 0.5 + 0.5i
+        b = 2*temp*b  + c_y;%0.15;
+    end
+    value = 1000;
